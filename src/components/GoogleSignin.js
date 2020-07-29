4016a570597d62401f9f5afaad6063c20a0cf610
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2020-07-29 23:48:00 +0700

    first commit

---
+++ src/components/GoogleSignin.js
@@ -0,0 +1,53 @@
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+import React, {useEffect, useState} from 'react';
+
+const GoogleButton = () => {
+  const [userInfo, setuserInfo] = useState({});
+  useEffect(() => {}, []);
+
+  const _signIn = async () => {
+    try {
+      GoogleSignin.configure({
+        // scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+        webClientId:
+          '202766130146-hir8v2igp1pasribup82slm0j08tnmlq.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+        // offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+        // hostedDomain: '', // specifies a hosted domain restriction
+        // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+        forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+        // accountName: '', // [Android] specifies an account name on the device that should be used
+        // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+      });
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('userInfo: ', userInfo);
+      setuserInfo(userInfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+      console.log('error: ', error);
+    }
+  };
+  return (
+    <GoogleSigninButton
+      style={{width: 192, height: 48}}
+      size={GoogleSigninButton.Size.Wide}
+      color={GoogleSigninButton.Color.Dark}
+      onPress={() => _signIn()}
+      // disabled={this.state.isSigninInProgress}
+    />
+  );
+};
+
+export default GoogleButton;
