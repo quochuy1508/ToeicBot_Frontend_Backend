153db4d76d24ab0b60d29baa807337cd1676cf04
Pham Thang <bachangngheo9a@gmail.com>
2020-07-31 15:32:16 +0700

    Attach interface

--- src/navigator/AuthNavigator.js
+++ src/navigator/AuthNavigator.js
@@ -8,7 +8,13 @@ const Stack = createStackNavigator();
 const AuthNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen 
+        name="Login" 
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack.Navigator>
   );
 };
--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,11 +1,24 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
-import GoogleSignin from '../../components/login/GoogleSignin';
+import React from 'react';
+import { View, Text} from 'react-native';
+import styles from './style';
+import Logo from '../../components/common/Logo';
+import GoogleSignin from '../../components/login/GoogleSignin'
 
 function Login() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <GoogleSignin />
+    <View style={styles.container}>
+      <Logo logosize={100} textsize={30} />
+      <View style={styles.buttonContainer}>
+        <GoogleSignin />
+      </View>
+      <View style={styles.supportContainer}>
+        <Text>Having trouble? </Text>
+        <Text style={styles.support}>Contact Support.</Text>
+      </View>
+      <View style={styles.signUpContainer}>
+        <Text>DON'T HAVE ANY ACCOUNTS? </Text>
+        <Text style={styles.signUp}>SIGN UP</Text>
+      </View>
     </View>
   );
 }
