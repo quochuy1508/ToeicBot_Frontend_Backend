a836ce6d7655c821558051e9e8df7451481d7090
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2020-08-29 17:11:51 +0700

    intergration dialogflow

---
+++ src/helpers/env.js
@@ -0,0 +1,16 @@
+// env.js
+
+export const dialogflowConfig = {
+  type: 'service_account',
+  project_id: 't-v-n-sskv',
+  private_key_id: '229ef250f79cadaf110c952b512c2599751c5792',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDaKQRLt6vN4NON\nxuslrDNRgZ2/f+FQo7JMNE7E2rR/oVCLhTtBmwnT50h2/Jw73bmDKdUCO6sQFsps\nhwwixJKAHgrM3Jd1ZZmHieTAndR4HBrev8QqoX96q+UAf+LUT5pA16s/ICXclxRs\nBZL6amzYVO4TnN3WoqHmma4Ucte33LeU0yB+QVgMd6hzM9HseKWkEzNsof2vk07J\nbjo0hVex50ejB+V3idHD0lXW3+YPmoWGV6gX+FU4Jkhdij07KRCx/x5JkXejKisg\nsE4+YOKctP4SXP3fe4McA0vTnSrJbJnJ++TVIZguxPQjhLxNI2UTTbKXdHRXA87Q\nAelljrutAgMBAAECggEAFWPv1blowTMkpbeV0dKhu4Gmengq8EcEpz/PuLHVqy1M\nIkn0xpjRggig4c3LRya/1vJQk5CkfrDatdZt+bC6yTto2q+xsriSBlNJGHEWkT+h\ni3/+V3cQnUdibkXxHdkU6lEF1kGBhNtUDMgPzQ7cpOolknW53fQsmgQtKI3Jb0T9\n/20HaZFBmduPRR7Y6GUtRjFze+hbNp/auIqBCKyQAWEqVX07TG7zTuXvXkYoTPE9\n3h6Og15DSBcj5blybmloMJ1pMYjCbfnnvhu+7lhaGboU+IFXFyVbeXIGi6J3CkOn\nZLKtANwL24HiwC1c4KeWXd9Q5fBRe78aPkbFW9j0fwKBgQD7iDSbvNYKXu8xMRcH\nfQ17It9DKpUlAOuZ8VHuRo7ps24VPj6XpgSO9yE1mLAb+iOm79cF33NQz4IxTp6a\nbkz85Gy9PJh55Hv/G1Nb7plibtw0nE1u1gvvq0i9hkgITHZTFWnY4Gampdd7RXwG\nic7SO+ZoQIPPNyToNK8jHKZ+QwKBgQDeCQ8mgWXCUo3K5myuNMWe7TfGo2kF2O2t\nRbL981SWzhGSwHymB3Pw8VWSTrZX4X4PtxSmofTQNJFQIiEPjAO6AS3wWQi+DXE/\nxiU3cTnvLmQCqrIlMq2Zy8EwgD8i809UlqzRN+P4HpZOlrsKlHe0S286P2m53xof\nRrKb5ddXTwKBgQCnup4KNBQu+jtafDe1IKi6sfhbW6v07vYsw4U09QwbvzDrLYSj\nr2tewmD80oKtRVI84bZrDiRhgN56bsjIhkEEanYwgpXSwpAE/33I0gSqZO6mjov8\nGvP+sCNjboXeMs68fI0GuCz7d5+HbkBUQ9UM0DDG2C6NIWKVDXy0V0iW7QKBgFuh\nVXVIlVwagZWoflVuRdgOgaQiL+dP3NZTG0keMFyD8lCjy00hakGW9v+IKEwgLMwD\nfCxATi3WY9m0G/y0hZ0ve2vi8m6sUX5GIXm96nDArllDZH1haq87nSCM7IFZ8/Uu\nsWcuL5Bn3xPTtKDrIHNxZhedr94bKkaaAH/B3JqVAoGBAM0UhzUUNSfDqQ5KagD2\nfJB7V6Q/McjX2xsuJsEQmeXSt6FxJeNLCXu+Bp8sLOzR88eXoz2cX/Btdj3ydDkD\nv0aFVhiG2DKqLpquux341elW31RA5pWa43bt0TakjnJk5AVyCODMIDr1YFuIHnYj\nJ4KxFAz71KijQv+wCBwHBoxU\n-----END PRIVATE KEY-----\n',
+  client_email: 'dialogflow-lsaqxu@t-v-n-sskv.iam.gserviceaccount.com',
+  client_id: '114164030330066574650',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-lsaqxu%40t-v-n-sskv.iam.gserviceaccount.com',
+};
