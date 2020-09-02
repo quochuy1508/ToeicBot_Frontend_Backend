e985a033c92872f41a980341373edf0a203c78b5
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2020-09-02 16:02:26 +0700

    Fix sort of message

--- src/screens/Chat/Chatbot.js
+++ src/screens/Chat/Chatbot.js
@@ -33,8 +33,19 @@ export default class Chatbot extends React.Component {
     this.renderBubble = this.renderBubble.bind(this);
     this.renderFooter = this.renderFooter.bind(this);
     this.onLoadEarlier = this.onLoadEarlier.bind(this);
+    this.compare = this.compare.bind(this);
 
     this._isAlright = null;
+  }
+
+  compare(a, b) {
+    if (a.createdAt > b.createdAt) {
+      return -1;
+    }
+    if (a.createdAt < b.createdAt) {
+      return 1;
+    }
+    return 0;
   }
 
   getData = async () => {
@@ -46,7 +57,7 @@ export default class Chatbot extends React.Component {
       .on('value', (snapshot) => {
         const value = Object.values(snapshot);
         if (value && Array.isArray(value) && value[0]['exists']) {
-          const messages = Object.values(value[0]['value']);
+          const messages = Object.values(value[0]['value']).sort(this.compare);
           console.log('messages: ', messages);
           this.setState(() => {
             return {
@@ -107,21 +118,22 @@ export default class Chatbot extends React.Component {
     //   };
     // });
     console.log('messages: ', messages);
-    // messages[0].sent = true;
-    // messages[0].received = true;
-    // messages[0].createdAt = new Date(messages[0].createdAt).getTime();
-    // const user = await AsyncStorage.getItem('user');
-    // await usersCollection.writeRecord(user, messages[0]);
+    messages[0].sent = true;
+    messages[0].received = true;
+    messages[0].createdAt = new Date(messages[0].createdAt).getTime();
+    const user = await AsyncStorage.getItem('user');
+    await usersCollection.writeRecord(user, messages[0]);
 
-    // let message = messages[0].text;
-    // await Dialogflow_V2.requestQuery(
-    //   message,
-    //   (result) => this.handleGoogleResponse(result),
-    //   (error) => console.log(error),
-    // );
+    let message = messages[0].text;
+    await Dialogflow_V2.requestQuery(
+      message,
+      (result) => this.handleGoogleResponse(result),
+      (error) => console.log(error),
+    );
   }
 
   async handleGoogleResponse(result) {
+    console.log(result);
     let text = result.queryResult.fulfillmentMessages[0].text.text[0];
     await this.onReceive(text);
   }
