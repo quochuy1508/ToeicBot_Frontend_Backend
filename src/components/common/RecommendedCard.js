7930d282c649f5d57261fee608d4ac6d4677953a
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2020-09-05 22:21:29 +0700

    Add recommed Link In Home page

--- src/components/common/RecommendedCard.js
+++ src/components/common/RecommendedCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, Image, Text, Linking } from 'react-native';
+import {StyleSheet, TouchableOpacity, Image, Text, Linking} from 'react-native';
 
 function Card({url, image, title}) {
   const styles = StyleSheet.create({
@@ -7,30 +7,34 @@ function Card({url, image, title}) {
       flex: 2,
       borderTopWidth: 2,
       borderTopColor: 'gainsboro',
+      marginBottom: 100,
     },
     image: {
-        height: 150,
+      height: 150,
     },
     title: {
-        padding: 20,
-        backgroundColor: 'gainsboro',
-        color: 'gray'
-    }
+      padding: 20,
+      backgroundColor: 'gainsboro',
+      color: 'gray',
+    },
   });
 
   const onPress = () => {
-    Linking.canOpenURL(url).then(supported => {
-        if (supported) {
-          Linking.openURL(url);
-        } else {
-          console.log(`Don't know how to open URL: ${url}`);
-        }
-      });
-  }
+    Linking.canOpenURL(url).then((supported) => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log(`Don't know how to open URL: ${url}`);
+      }
+    });
+  };
   return (
-    <TouchableOpacity style={styles.container} delayPressIn={0} onPress={() => onPress()}>
-        <Image source={{uri: image }} style={styles.image} resizeMode='cover'/>
-        <Text style={styles.title}>{title}</Text>
+    <TouchableOpacity
+      style={styles.container}
+      delayPressIn={0}
+      onPress={() => onPress()}>
+      <Image source={{uri: image}} style={styles.image} resizeMode="cover" />
+      <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   );
 }
--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,22 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View, StatusBar} from 'react-native';
 import styles from './style';
 import Card from '../../components/common/RecommendedCard';
+import AsyncStorage from '@react-native-community/async-storage';
+import usersCollection, {
+  database,
+} from '../../../server/collections/usersCollection';
 
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Đề xuất dành cho bạn</Text>
-      <Card  
-        url="https://www.anhngumshoa.com/tin-tuc/toeic-la-gi-bang-toeic-chung-chi-toeic-co-gia-tri-nhu-the-nao-36242.html"
-        image="https://llv.edu.vn/media/2017/11/logo-toeic-15440892075481729847449.jpg"
-        title="TOEIC là gì? Bằng TOEIC, chứng chỉ TOEIC có giá trị như thế nào?"
-      />
-      <View style={{flex: 3}}></View>
-      <StatusBar backgroundColor="deepskyblue"/>
-    </View>
-  );
+class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: [],
+    };
+
+    this._isMounted = false;
+  }
+
+  getData = async () => {
+    const user = JSON.parse(await AsyncStorage.getItem('user'));
+    await database
+      .ref(`/${user.id}-link`)
+      // .orderByChild('database/createdAt')
+      // .orderBy('createdAt', 'desc')
+      .on('value', (snapshot) => {
+        const value = Object.values(snapshot);
+        if (value && Array.isArray(value) && value[0]['exists']) {
+          // console.log('messages: ', messages);
+          this.setState(() => {
+            return {
+              links: Object.values(value[0]['value']),
+            };
+          });
+        } else {
+        }
+      });
+  };
+  UNSAFE_componentWillMount() {
+    this._isMounted = true;
+    this.getData();
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Đề xuất dành cho bạn</Text>
+        {this.state.links.map((link) => (
+          <Card
+            key={link['title']}
+            url={link['url']}
+            image={link['image']}
+            title={link['title']}
+          />
+        ))}
+
+        <View style={{flex: 3}}></View>
+        <StatusBar backgroundColor="deepskyblue" />
+      </View>
+    );
+  }
 }
 
 export default HomeScreen;
-
